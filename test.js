65d50093861e1cee8b9eea5a6b9021199493574b
Andres Mejias <aemabit@gmail.com>
2019-10-10 09:35:10 -0400

    test.js

--- test.js
+++ test.js
@@ -1,20 +1,61 @@
-/* first form*/
-const oneAverage = document.querySelector('#one');
-const btn = document.querySelector('#btnOne');
 
-function aveOne(event){
-    event.preventDefault()
+const btnOne = document.querySelector('#averageOne').getAttribute("disabled", "");
 
-    const maxAverage = 10;
-    const averageOne = Number(document.querySelector("#averageOne").value);
+// 
+//  const btnTwo = document.querySelector('#averageOne').getAttribute("disabled", "");
+//  const btnThree = document.querySelector('#averageOne').getAttribute("disabled", "");
+//  const btnFour = document.querySelector('#averageOne').getAttribute("disabled", "");
+// 
 
-    const totalAverage = maxAverage + averageOne;
-    const averageFinal = ( totalAverage / 2 );
-    console.log(averageOne)
+let btn = [ btnOne, btnTwo, btnThree, btnFour];
 
-    document.querySelector('#totalAverageOne').textContent = averageFinal.toFixed(2);
-    document.querySelector('#answer').classList.remove('hiddenOne');
+let oneAverageBtn, twoAverageBtn, threeAverageBtn, fourAverageBtn = false;
 
+
+function checkbtn(){
+    
+    if (oneAverage){
+        console.log("paso por el 1")
+    }
+    return oneAveragebtn = true;
 }
 
-oneAverage.addEventListener('submit', aveOne);
+function checkbtnTwo(){
+    
+    if (twoAverage){
+        console.log("paso por el 2")
+    }
+}
+function checkbtnThree(){
+    
+    if (threeAverage){
+        console.log("paso por el 3")
+    }
+    
+}
+function checkbtnFour(){
+    
+    if (fourAverage){
+        console.log("paso por el 4")
+    }
+    
+}
+
+ function allFunctionReady(checkbtn, checkbtnTwo, checkbtnThree, checkbtnFour){
+    
+    if (checkbtn && checkbtnTwo && checkbtnThree && checkbtnFour){
+        console.log("stay rdy")
+     }else{
+         console.log("not yet")
+     }
+ }
+
+ oneAverage.addEventListener('submit', checkbtn);
+ twoAverage.addEventListener('submit', checkbtnTwo);
+ threeAverage.addEventListener('submit', checkbtnThree);
+ fourAverage.addEventListener('submit', checkbtnFour);
+
+ oneAverage.addEventListener('submit', allFunctionReady);
+ twoAverage.addEventListener('submit', allFunctionReady);
+ threeAverage.addEventListener('submit', allFunctionReady);
+ fourAverage.addEventListener('submit', allFunctionReady);
